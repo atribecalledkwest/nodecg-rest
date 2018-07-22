ac93d1d85112b5e1a894649cd900baa39fc1f40f
Karen West <atribecalledkwest.97@gmail.com>
2018-07-22 08:06:21 -0400

    Changes to path names

--- extension/index.js
+++ extension/index.js
@@ -5,15 +5,23 @@ module.exports = (nodecg) => {
     let app = express();
     app.use(bodyparser.json());
 
-    app.get("/rest/message", (req, res) => {
-        nodecg.sendMessage(req.body["name"], req.body["data"]);
+    app.get("/rest/message/:name", (req, res) => {
+        if(req.body.hasOwnProperty("data")) {
+            nodecg.sendMessage(req.params.name, req.body["data"]);
+        } else {
+            nodecg.sendMessage(req.params.name);
+        }
         res.json({
             "status": "OK"
         });
     });
 
-    app.get("/rest/message/:bundleName", (req, res) => {
-        nodecg.sendMessageToBundle(req.body["name"], req.params.bundleName, req.body["data"]);
+    app.get("/rest/message/:bundleName/:name", (req, res) => {
+        if(req.body.hasOwnProperty("data")) {
+            nodecg.sendMessageToBundle(req.params.name, req.params.bundleName, req.body["data"]);    
+        } else {
+            nodecg.sendMessageToBundle(req.params.name, req.params.bundleName);
+        }
         res.json({
             "status": "OK"
         });
